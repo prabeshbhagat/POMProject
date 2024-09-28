91d5a9f13526864711995b3285b9aab32da30e07
PRITI KUMARI <prabeshctc@gmail.com>
2024-09-28 13:01:38 +0530

    1st commit

---
+++ src/main/java/com/qa/opencart/factory/OptionsManager.java
@@ -0,0 +1,67 @@
+package com.qa.opencart.factory;
+
+import java.util.Properties;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+//POM 6 30 misn
+public class OptionsManager {
+
+	private Properties prop;
+	private ChromeOptions co;
+	private FirefoxOptions fo;
+	private EdgeOptions eo;
+	
+	public OptionsManager(Properties prop) {
+		this.prop = prop;
+	}
+
+	public ChromeOptions getChromeOptions() {
+		co = new ChromeOptions();
+		if (Boolean.parseBoolean(prop.getProperty("headless"))) {
+			System.out.println("<--------Running the test in Headless Mode------>");
+			co.addArguments("--headless");
+		}
+		if (Boolean.parseBoolean(prop.getProperty("incognito"))) {
+			System.out.println("<----Running the test in iNCOGNIto Mode--->>");
+			co.addArguments("--incognito");
+		}
+
+		return co;
+	}
+
+	public FirefoxOptions getFirefoxOptions() {
+		fo = new FirefoxOptions();
+		if (Boolean.parseBoolean(prop.getProperty("headless"))) {
+			System.out.println("<--------Running the test in Headless Mode------>");
+			fo.addArguments("--headless");
+		}
+		if (Boolean.parseBoolean(prop.getProperty("incognito"))) {
+			System.out.println("<----Running the test in iNCOGNIto Mode--->>");
+			fo.addArguments("--incognito");
+		}
+
+		return fo;
+	}
+	
+	public EdgeOptions getEdgeOptions() {
+		eo = new EdgeOptions();
+
+		if (Boolean.parseBoolean(prop.getProperty("headless"))) {
+			System.out.println("====Running tests in headless======");
+			eo.addArguments("--headless");
+		}
+		if (Boolean.parseBoolean(prop.getProperty("incognito"))) {
+			eo.addArguments("--inPrivate");
+		}
+
+		if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+			eo.setCapability("browserName", "edge");
+			// eo.setCapability("enableVNC", true);
+		}
+
+		return eo;
+	}
+
+}
---
+++ src/main/java/com/qa/opencart/pages/test.java
@@ -0,0 +1,23 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class test {
+	
+	WebDriver driver= new ChromeDriver();
+	
+	
+	
+	@Test
+	public void m1() {
+		
+		System.out.println("Test123");
+		
+	}
+
+}
